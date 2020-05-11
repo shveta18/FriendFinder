66ee2901c4a758193f0ce8915f756f53989668d7
Shveta Kumar <sk@shveta.ca>
2020-05-11 13:51:35 -0400

    added character list and scores to friends.js file

---
+++ app/data/friends.js
@@ -0,0 +1,27 @@
+var matchList = [
+    {
+        "name": "Arya",
+        "photo": "https://en.wikipedia.org/wiki/Arya_Stark#/media/File:Arya_Stark-Maisie_Williams.jpg",
+        "scores": [5, 0, 1, 0, 0, 0, 5, 0, 0, 0]
+    },
+    {
+        "name": "Tyrion",
+        "photo": "https://en.wikipedia.org/wiki/Arya_Stark#/media/File:Arya_Stark-Maisie_Williams.jpg",
+        "scores": [0, 5, 0, 0, 0, 2, 3, 5, 2, 0]
+    },
+    {
+        "name": "Petyr",
+        "photo": "https://en.wikipedia.org/wiki/Arya_Stark#/media/File:Arya_Stark-Maisie_Williams.jpg",
+        "scores": [0, 2, 0, 0, 5, 0, 3, 0, 0, 0]
+    },
+    {
+        "name": "Varys",
+        "photo": "https://en.wikipedia.org/wiki/Arya_Stark#/media/File:Arya_Stark-Maisie_Williams.jpg",
+        "scores": [0, 3, 0, 0, 2, 5, 4, 0, 5, 0]
+    },
+    {
+        "name": "John",
+        "photo": "https://en.wikipedia.org/wiki/Arya_Stark#/media/File:Arya_Stark-Maisie_Williams.jpg",
+        "scores": [0, 2, 5, 5, 0, 0, 3, 0, 0, 0]
+    },
+]

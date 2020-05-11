c50a331505cec1a91ada27dd55e0c8fe8c73ae1e
Shveta Kumar <sk@shveta.ca>
2020-05-11 18:03:44 -0400

    Able to grab values selected by the user and store into an array

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,14 @@
-var matchListArray = require("./../data/friends");
+var friendsArray = require("./../data/friends");
 module.exports =function(app) {
 // display json of all possible friends
 app.get("/friends", function(request, response) {
-  response.json(matchListArray);
+  response.json(friendsArray);
 });
 
+
+// add POST code to handle survey results
+
+app.post("/friends", function(request, response){
+  response.json()
+})
 }

c9ccb1dc7b30d368ecd17c8cf53d11f657c3249e
Shveta Kumar <sk@shveta.ca>
2020-05-12 16:47:16 -0400

    added psuedocode for calculating the best match

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -10,6 +10,16 @@ app.get("/friends", function(request, response) {
 
 app.post("/friends", function(request, response){
   friendsArray.push(request.body);
-  response.json()
+  // How to calculate the match based on user submissions: 
+  // 1. grab each user in the list and get the scores array
+  // 2. get the user's score and store in an array
+  // 3. get the difference between each number for the two users and store in an array 
+  // 4. if anything is a negative, convert to positive
+  // 5. add up the numbers and store in an array
+  // 6. travers the array and pick which position has the lowest score
+  // 7. pull the best match for the user in that position
+
+ 
+  response.json(friendsArray);
 })
 } 

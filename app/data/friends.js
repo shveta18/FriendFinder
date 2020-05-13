62c33f0fb5e8897517b3a7ba779f8f97cc57cacf
Shveta Kumar <sk@shveta.ca>
2020-05-12 21:59:41 -0400

    Made some modifications to the questions and the scores array to better represent weightage of the answers

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,27 +2,27 @@ var matchListArray = [
     {
         "name": "Arya",
         "photo": "https://upload.wikimedia.org/wikipedia/en/3/39/Arya_Stark-Maisie_Williams.jpg",
-        "scores": [5, 0, 1, 0, 0, 0, 5, 0, 0, 0]
+        "scores": [5, 0, 3, 0, 0, 0, 5, 0, 0, 0]
     },
     {
         "name": "Tyrion",
         "photo": "https://upload.wikimedia.org/wikipedia/en/5/50/Tyrion_Lannister-Peter_Dinklage.jpg",
-        "scores": [0, 5, 0, 0, 0, 2, 3, 5, 2, 0]
+        "scores": [2, 5, 0, 0, 2, 2, 3, 5, 2, 2]
     },
     {
         "name": "Petyr",
         "photo": "https://upload.wikimedia.org/wikipedia/en/d/d5/Aidan_Gillen_playing_Petyr_Baelish.jpg",
-        "scores": [0, 2, 0, 0, 5, 0, 3, 0, 0, 0]
+        "scores": [5, 2, 0, 0, 5, 0, 3, 2, 0, 5]
     },
     {
         "name": "Varys",
         "photo": "https://upload.wikimedia.org/wikipedia/en/0/01/Varys-Conleth_Hill.jpg",
-        "scores": [0, 3, 0, 0, 2, 5, 4, 0, 5, 0]
+        "scores": [2, 3, 0, 0, 2, 5, 4, 3, 5, 2]
     },
     {
         "name": "John",
         "photo": "https://upload.wikimedia.org/wikipedia/en/3/30/Jon_Snow_Season_8.png",
-        "scores": [0, 2, 5, 5, 0, 0, 3, 0, 0, 0]
+        "scores": [0, 2, 5, 5, 0, 0, 3, 2, 0, 0]
     },
 ]
 // export the array so it is available on other .js files
